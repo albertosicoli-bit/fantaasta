00abb5fa4b8d03958813e34b8585512af49b9136
albertosicoli-bit <alberto.sicoli@gmail.com>
2026-09-04 00:22:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='fantaasta-excel-user-v13';
+const C='fantaasta-goal-assist-v14';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
