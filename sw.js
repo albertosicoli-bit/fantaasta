00c2656d36b1345fac82e71c12213a3aebf21d95
albertosicoli-bit <alberto.sicoli@gmail.com>
2026-09-04 02:08:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-const C='fantaasta-goal-assist-v14';
+const C='fantaasta-sort-goals-fm-v15';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  self.clients.claim(),
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-])));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
  if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{
-    const copy=resp.clone();caches.open(C).then(c=>c.put('./index.html',copy));return resp;
-  }).catch(()=>caches.match('./index.html')));
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(resp=>{const copy=resp.clone();caches.open(C).then(c=>c.put('./index.html',copy));return resp}).catch(()=>caches.match('./index.html')));
  }else{
-  e.respondWith(fetch(e.request).then(resp=>{
-    const copy=resp.clone();caches.open(C).then(c=>c.put(e.request,copy));return resp;
-  }).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(C).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request)));
  }
 });
